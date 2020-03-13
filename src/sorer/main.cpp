1a4e43961a882da08b978c1b02558308007f8689
chasebroder <chaseb712@gmail.com>
2020-03-13 18:26:17 -0400

    Some changes to main file

--- src/sorer/main.cpp
+++ src/sorer/main.cpp
@@ -1,23 +1,5 @@
 //lang::Cpp
 
-
-/**
- * sorer: This program reads a file passed in with -f option.
- *
- * Optionally, -from tells you the location where you start the read in the file
- *             -len tells you how many bytes to read from the file
- *             -print_col_type asks for the type of the col indicated
- *             -print_col_idx asks for the value of the field at the col, idx
- *             -is_missing_idx asks whether the field at col, idx is an empty/missing value
- *
- * Unless, -print_col_type, print_col_idx, is_missing_idx options are used, nothing will happen.
- *
- * If those options are used, a -f must be included.
- *
- * If no -len or no -from options are given, we assume the whole file must be read.
- */
-
-
 #include <iostream>
 #include <cassert>
 #include <cstdio>
@@ -44,6 +26,8 @@ const char *USAGE = "Usage: ./sorer [-f] [-from] [-len] [-print_col_type] " \
              "Only one option of each kind can be used.\n";
 
 
+/** Entrypoint for sorer program. Sorer used to read in data and then convert
+ * it to a DataFrame */
 int main(int argc, char **argv) {
     Sorer* s = new Sorer(argv[1]);
     DataFrame* d = s->getFrame();
