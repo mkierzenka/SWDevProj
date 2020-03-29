a695cbd8a9c2fe716317af2182ee106d8051f9dd
chasebroder <chaseb712@gmail.com>
2020-03-29 18:48:09 -0400

    Added methods for retrieving properties of application

--- src/application/application.h
+++ src/application/application.h
@@ -16,11 +16,13 @@ class Application : public Object
 {
 public:
     size_t idx_; //what index node this application is running on
+    PseudoNetwork* net_; //network for sending and receiving messages
     KVStore* kv_;
 
     Application(size_t i, PseudoNetwork* net)
     {
         idx_ = i;
+        net_ = net;
         kv_ = new KVStore(i, net);
     }
 	
@@ -31,6 +33,20 @@ public:
     size_t this_node()
     {
         return idx_;
+    }
+
+    /** Get network from this application. Some threads that work on this application
+     * may need it.
+     */
+    PseudoNetwork* getNetwork()
+    {
+        return net_;
+    }
+
+    /** Get store from application */
+    KVStore* getStore()
+    {
+        return kv_;
     }
 
     virtual void run_() {}
