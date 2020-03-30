6536a97dc56f9a2128463ca9a4b59d069f224450
chasebroder <chaseb712@gmail.com>
2020-03-30 17:32:18 -0400

    Updated print statements

--- src/network/pseudo/pseudonetwork.h
+++ src/network/pseudo/pseudonetwork.h
@@ -36,10 +36,10 @@ public:
     void sendMsg(Message *m)
     {
         size_t target = m->getTarget();
-        printf("SENDER: %zu\n", m->getSender());
-        printf("TARGET: %zu\n", target);
+        //printf("SENDER: %zu\n", m->getSender());
+        //printf("TARGET: %zu\n", target);
         mqa_->get(target)->push(m);
-        printf("Message sent to %zu\n", target);
+        printf("Message sent from %zu to %zu\n", m->getSender(), target);
         printf("Length of message queue %zu's message array: %zu\n", target, mqa_->get(target)->size());
     }
 
