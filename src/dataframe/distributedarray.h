5533ff0f281ecd27fddae96eeaf741562b848b20
chasebroder <chaseb712@gmail.com>
2020-03-30 17:30:39 -0400

    Both local and network retrieval controlled by getValue

--- src/dataframe/distributedarray.h
+++ src/dataframe/distributedarray.h
@@ -233,13 +233,9 @@ public:
 	 *  Caller is expected to delete the block
 	 */
 	DoubleBlock* getDoubleBlockFromStore_(Key* k) {
+        printf("Getting block with key %s\n", k->kStr_->c_str());
 		Value* val = store_->getValue(k);
-        // assert(val != nullptr);
-        if (val == nullptr)
-        {
-            printf("Getting block from network\n");
-            val = store_->getValueFromNetwork(k);
-        }
+        assert(val != nullptr);
 		Serializer* s = new Serializer(val->getSize(), val->getData());
 		DoubleBlock* out = new DoubleBlock();
 		out->deserialize(s);
