ce84c9c52e2dca8ee80be2906c37f73919c90955
Marcin Kierzenka <33709159+mkierzenka@users.noreply.github.com>
2020-04-01 16:02:14 -0400

    Removed excess from demo application

--- src/application/demo.h
+++ src/application/demo.h
@@ -32,35 +32,29 @@ public:
   }
  
   void producer() {
-	printf("Producer starting\n");
-    size_t SZ = 10;//100*1000
+    pln("Producer Started");
+    size_t SZ = 100*1000;
     double* vals = new double[SZ];
     double sum = 0;
     for (size_t i = 0; i < SZ; ++i) sum += vals[i] = i;
     DataFrame::fromArray(main, kv_, SZ, vals);
     DataFrame::fromScalar(check, kv_, sum);
+    pln("Producer Finished");
   }
  
   void counter() {
-	printf("Counter starting\n");
+    pln("Counter Started");
     DataFrame* v = kv_->waitAndGet(main);
-    printf("Counter got df\n");
-    double sum = 0;
-    for (size_t i = 0; i < 10; ++i) {
-		sum += v->get_double(0,i);
-		printf("Counter considered %zu\n", i);
-	}
-    //for (size_t i = 0; i < 100*1000; ++i) sum += v->get_double(0,i);
+    size_t sum = 0;
+    for (size_t i = 0; i < 100*1000; ++i) sum += v->get_double(0,i);
     p("The sum is  ").pln(sum);
     DataFrame::fromScalar(verify, kv_, sum);
-    printf("Counter loop done\n");
+    pln("Counter Finished");
   }
  
   void summarizer() {
-	printf("Summarizer starting\n");
+    pln("Summarizer Finished");
     DataFrame* result = kv_->waitAndGet(verify);
-    printf("Verify key: %lf\n", result->get_double(0, 0));
-    //p("Verify key:").pln(result->get_double(0, 0));
     DataFrame* expected = kv_->waitAndGet(check);
     pln(expected->get_double(0,0)==result->get_double(0,0) ? "SUCCESS":"FAILURE");
   }
