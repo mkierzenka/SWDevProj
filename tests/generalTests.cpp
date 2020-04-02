5db75e6e40f9c03cc8e7b7a3393fc9bd7856d3d0
chasebroder <chaseb712@gmail.com>
2020-04-02 14:59:33 -0400

    Starting to clean up some include statements

--- tests/generalTests.cpp
+++ tests/generalTests.cpp
@@ -6,6 +6,10 @@
 #include "../src/dataframe/sumrower.h"
 #include "../src/dataframe/findrower.h"
 #include "../src/dataframe/lengthrower.h"
+#include "../src/store/key.h"
+#include "../src/serial/serial.h"
+#include "../src/utils/object.h"
+#include "../src/utils/string.h"
 
 Sys *SYSTEM = new Sys();
 
