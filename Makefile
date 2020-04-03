75fd423d3c415c97b4c3fb38856aa171e392d9f7
chasebroder <chaseb712@gmail.com>
2020-04-02 20:44:22 -0400

    Fixed cache tests to pass (new method sig). Restructed Makefile a little

--- Makefile
+++ Makefile
@@ -1,17 +1,14 @@
-build:
+build: buildTrivial buildDemo
 	g++ --std=c++11 -g -pthread tests/generalTests.cpp -o tests/testGeneral
 	g++ --std=c++11 -g tests/serializationTests.cpp -o tests/testSerial
-	g++ --std=c++11 -g -pthread tests/trivialTest.cpp -o tests/testTrivial
-	g++ -std=c++11 -pthread -g tests/demoTest.cpp -o tests/testDemo
 	g++ --std=c++11 -g tests/mapTest.cpp -o tests/testMap
 	g++ --std=c++11 -g tests/arrayTest.cpp -o tests/testArray
 	g++ --std=c++11 -g tests/queueTest.cpp -o tests/testQueue
+	g++ --std=c++11 -g tests/cacheTest.cpp -o tests/testCache
 	#g++ --std=c++11 -g tests/memTest.cpp -o tests/testMemory
 	#g++ --std=c++11 -g tests/sorerTest.cpp -o tests/testSorer
 
-test:
-	./tests/testTrivial
-	./tests/testDemo
+test: testTrivial testDemo
 
 ourTests:
 	./tests/testSerial
@@ -19,6 +16,7 @@ ourTests:
 	./tests/testMap
 	./tests/testArray
 	./tests/testQueue
+	./tests/testCache
 	#./tests/testMemory
 	#./tests/testSorer data/datafile.sor
 	
@@ -26,31 +24,21 @@ memory:
 	#valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes ./tests/testSorer data/datafile.sor
 	#valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes ./tests/testTrivial
 	valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes ./tests/testDemo
-.SILENT: clean
-clean:
-	rm -f tests/testGeneral tests/testCA tests/testMemory tests/testSorer tests/testMap tests/testArray \
-	tests/testQueue tests/testSerial tests/testTrivial tests/testCache tests/testDemo
-
-buildCache:
-	g++ -std=c++11 -g tests/cacheTest.cpp -o tests/testCache
-
-testCache:
-	./tests/testCache
 
 buildTrivial:
-	g++ -std=c++11 -g -pthread tests/trivialTest.cpp -o tests/testTrivial
+	g++ --std=c++11 -g -pthread tests/trivialTest.cpp -o tests/testTrivial
 
 testTrivial:
 	./tests/testTrivial
 
 buildDemo:
-	g++ -std=c++11 -pthread -g tests/demoTest.cpp -o tests/testDemo
+	g++ --std=c++11 -g -pthread tests/demoTest.cpp -o tests/testDemo
 
 testDemo:
 	./tests/testDemo
 
-buildGeneral:
-	g++ -std=c++11 -pthread -g tests/generalTests.cpp -o tests/testGeneral
 
-testGeneral:
-	./tests/testGeneral
+.SILENT: clean
+clean:
+	rm -f tests/testGeneral tests/testCA tests/testMemory tests/testSorer tests/testMap tests/testArray \
+	tests/testQueue tests/testSerial tests/testTrivial tests/testCache tests/testDemo
--- tests/cacheTest.cpp
+++ tests/cacheTest.cpp
@@ -1,11 +1,17 @@
 #include "../src/dataframe/cache.h"
 #include "../src/store/key.h"
 #include "../src/store/value.h"
+#include "../src/dataframe/block.h"
+#include "../src/dataframe/intblock.h"
+#include "../src/dataframe/doubleblock.h"
+#include "../src/dataframe/boolblock.h"
+#include "../src/dataframe/stringblock.h"
 
 #include <assert.h>
 
 int main()
 {
+    printf("Cache test starting...\n");
     //create cache and some elements to put into the cache
     Cache* c = new Cache(3);
 
@@ -16,38 +22,73 @@ int main()
     Key* key4 = new Key("key4", 3);    
     Key* key5 = new Key("key5", 2);
 
-    Value* val1 = new Value("value1", 7);
-    Value* val2 = new Value("value 2", 8);
-    Value* val3 = new Value("val3", 5);
-    Value* val4 = new Value("v4", 3); 
-    Value* val5 = new Value("v5", 1);
+    IntBlock* ib = new IntBlock();
+    ib->add(17);
+    ib->add(44);
+    ib->add(123);
+
+    StringBlock* sb = new StringBlock();
+    String* s1 = new String("value1");
+    String* s2 = new String("value 2");
+    String* s3 = new String("james");
+    String* s4 = new String("bond");
+    String* s5 = new String("1234");
+    sb->add(s1);
+    sb->add(s2);
+    sb->add(s3);
+    sb->add(s4);
+    sb->add(s5);
+
+    BoolBlock* bb = new BoolBlock();
+    bb->add(true);
+    bb->add(true);
+    bb->add(false);
+
+    DoubleBlock* db = new DoubleBlock();
+    db->add(1.92);
+    db->add(0.0);
+    db->add(7.777);
+    db->add(0.99999);
+
+    IntBlock* ib2 = new IntBlock();
+    ib2->add(3);
+    ib2->add(88888);
+    ib2->add(229375);
 
     //add pairings to cache
-    c->put(key1, val1);
-    c->put(key2, val2);
-    c->put(key3, val3);
+    c->put(key1, ib);
+    c->put(key2, sb);
+    c->put(key3, bb);
 
     assert(c->keyOrder_->size() == 3);
 
     assert(c->containsKey(key1));
+    assert(c->getBlock(key1)->equals(ib));
     assert(c->containsKey(key2));
+    assert(c->getBlock(key2)->equals(sb));
     assert(c->containsKey(key3));
+    assert(c->getBlock(key3)->equals(bb));
     assert(c->keyOrder_->size() == 3);
     assert(c->data_->size() == 3);
     assert(!c->containsKey(key4));
 
-    c->put(key4, val4);
-    assert(!c->containsKey(key1));
+    c->put(key4, db);
+    assert(!c->containsKey(key1)); //because we dropped it (Cache size = 3)
     assert(c->containsKey(key4));
+    assert(c->getBlock(key4)->equals(db));
 
-    c->put(key5, val5);
+    c->put(key5, ib2);
     assert(!c->containsKey(key1));
+    assert(!c->containsKey(key2));
+    assert(c->containsKey(key3));
+    assert(c->getBlock(key3)->equals(bb));
     assert(c->containsKey(key4));
+    assert(c->getBlock(key4)->equals(db));
     assert(c->containsKey(key5));
+    assert(c->getBlock(key5)->equals(ib2));
 
-    
-    //delete allocated memory
     delete c;
     delete key1, key2, key3, key4, key5;
-    delete val1, val2, val3, val4, val5;
+    delete ib, sb, bb, db, ib2;
+    printf("Cache test passed!\n");
 }
