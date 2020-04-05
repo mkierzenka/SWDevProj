17f5b8178b3232b1eb3efde54053adeca60f7b73
chasebroder <chaseb712@gmail.com>
2020-04-05 19:09:34 -0400

    Directory now holds addresses as a String

--- src/network/directory.h
+++ src/network/directory.h
@@ -1,9 +1,14 @@
+//lang:Cpp
+
 #pragma once
 
 #include "../utils/object.h"
+#include "../utils/string.h"
 #include "../utils/map.h"
 #include "../serial/serial.h"
-#include "Integer.h"
+#include "sizetwrapper.h"
+
+#include <arpa/inet.h>
 
 /**
  * This class represents a node's directory, maintaining a list of its 
@@ -14,7 +19,7 @@
 class Directory : public Object
 {
 public:
-    Map *ips_; //map of node id to ip
+    Map *ips_; //map of node id to ip address
 
     Directory()
     {
@@ -29,48 +34,79 @@ public:
     /**
      * Add a new node id and ip address to this directory
      */
-    void addIp(size_t nodeId, String *ipAddr)
+    void addIp(size_t nodeId, String *addr)
     {
-        Integer *id = new Integer((int)nodeId);
-        ips_->put(id->clone(), ipAddr->clone());
+        SizeTWrapper *id = new SizeTWrapper(nodeId);
+        ips_->put(id->clone(), addr->clone());
         delete id;
     }
 
     /** Get ip with the given node id */
-    String *getAddress(size_t nodeId)
+    char* getAddress(size_t nodeId)
     {
-        Integer *node = new Integer(nodeId);
+        SizeTWrapper *node = new SizeTWrapper(nodeId);
         String *addr = dynamic_cast<String *>(ips_->get(node));
         delete node;
-        return addr;
+        return addr->c_str();
     }
 
     /** Clone this directory */
-    Directory* clone()
-    {
-        Directory* res = new Directory;
-        Object** keys = ips_->get_keys();
-        //add all values to new directory
-        for (size_t i = 0; i < ips_->size(); i++)
-        {
-            Integer* node = dynamic_cast<Integer*>(keys[i]);
-            String* ip = dynamic_cast<String*>(ips_->get(node));
-            res->addIp(node->asUnsignedInt(), ip->clone());
-        }
+    // Directory* clone()
+    // {
+    //     Directory* res = new Directory;
+    //     Object** keys = ips_->get_keys();
+    //     //add all values to new directory
+    //     for (size_t i = 0; i < ips_->size(); i++)
+    //     {
+    //         Integer* node = dynamic_cast<Integer*>(keys[i]);
+    //         String* ip = dynamic_cast<String*>(ips_->get(node));
+    //         res->addIp(node->asUnsignedInt(), ip->clone());
+    //     }
 
-        //check for equality before returning?
-        return res;
-    }
+    //     //check for equality before returning?
+    //     return res;
+    // }
 
     /** Serialize this directory */
-    void serialize(Serializer* s)
+    void serialize(Serializer *s)
     {
-        ips_->serialize(s);
+        //write size and capacity
+        s->write(ips_->size());
+        s->write(ips_->capacity());
+
+        Object **keys = ips_->get_keys();
+        //serialize all key-value pairings
+        for (size_t i = 0; i < ips_->size(); i++)
+        {
+            SizeTWrapper *node = dynamic_cast<SizeTWrapper *>(keys[i]);
+            String *ip = dynamic_cast<String *>(ips_->get(node));
+
+            node->serialize(s);
+            ip->serialize(s);
+        }
     }
 
     /** Deserialize this directory */
-    void deserialize(Serializer* s)
+    void deserialize(Serializer *s)
     {
-        ips_->deserialize(s);
+        //read size and capacity
+        size_t size = s->readSizeT();
+        size_t capacity = s->readSizeT();
+
+        //initialize map with capacity
+        ips_ = new Map(capacity);
+
+        SizeTWrapper *node = new SizeTWrapper();
+        String *addr = new String();
+        //deserialize all items and put them in the map
+        for (size_t i = 0; i < size; i++)
+        {
+            node->deserialize(s);
+            addr->deserialize(s);
+            ips_->put(node->clone(), addr->clone());
+        }
+
+        delete node;
+        delete addr;
     }
 };
