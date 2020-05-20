b9d0b1dc70d37d1337ad62ad4b2aad42c6602360
Marcin Kierzenka <33709159+mkierzenka@users.noreply.github.com>
2020-05-20 13:14:31 -0400

    Fixed memory leak in Summer and reformmatted the file

--- src/filereader/summer.h
+++ src/filereader/summer.h
@@ -9,26 +9,33 @@
 class Summer : public Writer {
 public:
   SIMap& map_;
-  String** strs_;
+  String** strs_;          //owned
   size_t size_;
   size_t curIdx_;
  
-   Summer(SIMap& map) : map_(map) {
-       strs_ = map.get_str_keys();
-       size_ = map.size();
-       curIdx_ = 0;
-   }
+  Summer(SIMap& map) : map_(map) {
+    strs_ = map.get_str_keys();
+    size_ = map.size();
+    curIdx_ = 0;
+  }
+
+  ~Summer() {
+    for (size_t i = 0; i < size_; i++) {
+      delete strs_[i];
+	}
+	delete[] strs_;
+  }
 
   void visit(Row& r) {
-      String* key = strs_[curIdx_];
-      Num* val = (Num*)(map_.get(key));
-      size_t valInt = val->asInt();
-      r.set(0, key);
-      r.set(1, (int) valInt);
-      curIdx_++;
+    String* key = strs_[curIdx_];
+    Num* val = (Num*)(map_.get(key));
+    size_t valInt = val->asInt();
+    r.set(0, key);
+    r.set(1, (int) valInt);
+    curIdx_++;
   }
  
   bool done() {
-        return curIdx_ == map_.size();
-    }
+    return curIdx_ == map_.size();
+  }
 };
