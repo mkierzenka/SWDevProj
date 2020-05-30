5382b0ae02d24b1907c30692a98e1a17ed27305e
Marcin Kierzenka <33709159+mkierzenka@users.noreply.github.com>
2020-05-30 11:54:25 -0400

    Reformatted comments in map.h

--- src/utils/map.h
+++ src/utils/map.h
@@ -37,21 +37,21 @@ public:
 	std::unordered_map<Object*, Object*, myMapHash, myMapEquals> inner_;
 
 	/**
-		 * @brief Construct a new Map object
-		 * 
-		 */
+	 * @brief Construct a new Map object
+	 * 
+	 */
 	Map() : Map(4) {}
 
 	/**
-		 * Constructor to optimize performance of map. !!Argument is ignored!!
-		 * @param capacity number of buckets available to store different values.
-		 */
+	 * Constructor to optimize performance of map. !!Argument is ignored!!
+	 * @param capacity number of buckets available to store different values.
+	 */
 	Map(size_t capacity) : Object() {}
 
 	/**
-		 * @brief Destroy the Map object
-		 * 
-		 */
+	 * @brief Destroy the Map object
+	 * 
+	 */
 	~Map()
 	{
 		deleteData_();
@@ -73,13 +73,13 @@ public:
 	}
 
 	/**
-		 * @brief Determines if this map equals the given object. For two maps to be equal, they must
-		 * contain the exact same key-value pairs, BUT do not need to share the same locations inside
-		 * of the buckets.
-		 * 
-		 * @param other object to compare to
-		 * @return equals or not
-		 */
+	 * @brief Determines if this map equals the given object. For two maps to be equal, they must
+	 * contain the exact same key-value pairs, BUT do not need to share the same locations inside
+	 * of the buckets.
+	 * 
+	 * @param other object to compare to
+	 * @return equals or not
+	 */
 	virtual bool equals(Object *other)
 	{
 		if (this == other)
@@ -146,20 +146,20 @@ public:
 	}
 
 	/**
-		 * @brief Calculates the hash of the map. The hash will only be dependent on the key value pairs
-		 * of the map, and NOT the location of them in the buckets.
-		 * Example:
-		 * Map* map1 = new Map(10000);
-		 * Map* map2 = new Map();
-		 * Object* key = new Object();
-		 * Object* value = new Object();
-		 * map1->put(key, value);
-		 * map2->put(key, value);
-		 * // This should return true even though the key value pairs are in different bucket locations
-		 * map1->hash() == map2->hash(); 
-		 * 
-		 * @return hash representation of each key value pair in the map
-		 */
+	 * @brief Calculates the hash of the map. The hash will only be dependent on the key value pairs
+	 * of the map, and NOT the location of them in the buckets.
+	 * Example:
+	 * Map* map1 = new Map(10000);
+	 * Map* map2 = new Map();
+	 * Object* key = new Object();
+	 * Object* value = new Object();
+	 * map1->put(key, value);
+	 * map2->put(key, value);
+	 * // This should return true even though the key value pairs are in different bucket locations
+	 * map1->hash() == map2->hash(); 
+	 * 
+	 * @return hash representation of each key value pair in the map
+	 */
 	virtual size_t hash()
 	{
 		Object **keys = this->get_keys();
@@ -181,34 +181,34 @@ public:
 	}
 
 	/**
-		 * @brief Determines the number of mappings in this map.
-		 * @return size of map
-		 */
+	 * @brief Determines the number of mappings in this map.
+	 * @return size of map
+	 */
 	virtual size_t size()
 	{
 		return inner_.size();
 	}
 
 	/**
-		 * Return capacity of the map
-		 */
+	 * Return capacity of the map
+	 */
 	size_t capacity()
 	{
 		return inner_.max_size();
 	}
 
 	/**
-		 * Adds a mapping from key to val to the map. If there is already a key
-		 * of the same value in the map, the value will be overwritten by the new value.
-		 * Caller is responsible for deleting returned value.
-		 * If the key already exists in the map, caller responsible for deleting key.
-		 * 
-		 * NOTE: This function will mutate the Map
-		 * 
-		 * @param key Object key
-		 * @param val Object value
-		 * @return the old value from the given key, a nullptr is returned if no old value exists
-		 */
+	 * Adds a mapping from key to val to the map. If there is already a key
+	 * of the same value in the map, the value will be overwritten by the new value.
+	 * Caller is responsible for deleting returned value.
+	 * If the key already exists in the map, caller responsible for deleting key.
+	 * 
+	 * NOTE: This function will mutate the Map
+	 * 
+	 * @param key Object key
+	 * @param val Object value
+	 * @return the old value from the given key, a nullptr is returned if no old value exists
+	 */
 	Object *put(Object *key, Object *val)
 	{
 		if (!key || !val)
@@ -227,11 +227,11 @@ public:
 	}
 
 	/**
-		 * Returns the actual object that the key maps to.
-		 * 
-		 * @param key Object unique key
-		 * @return Object value for the key, a nullptr is returned if no value exists
-		 */
+	 * Returns the actual object that the key maps to.
+	 * 
+	 * @param key Object unique key
+	 * @return Object value for the key, a nullptr is returned if no value exists
+	 */
 	Object *get(Object *key)
 	{
 		if (!key || !contains_key(key))
@@ -242,12 +242,12 @@ public:
 	}
 
 	/**
-		 * Removes the key value mapping from the map.
-		 * Caller responsible for deleting returned value.
-		 * 
-		 * @param key Object key of mapping to remove
-		 * @return value that was removed, a nullptr is returned if no value exists
-		 */
+	 * Removes the key value mapping from the map.
+	 * Caller responsible for deleting returned value.
+	 * 
+	 * @param key Object key of mapping to remove
+	 * @return value that was removed, a nullptr is returned if no value exists
+	 */
 	Object *remove(Object *key)
 	{
 		if (!key)
@@ -268,10 +268,10 @@ public:
 	}
 
 	/**
-		 * Determines if the map contains the given key.
-		 * @param key Object key
-		 * @return if the key is in the map
-		 */
+	 * Determines if the map contains the given key.
+	 * @param key Object key
+	 * @return if the key is in the map
+	 */
 	bool contains_key(Object *key)
 	{
 		if (!key)
@@ -282,8 +282,8 @@ public:
 	}
 
 	/**
-		 * Remove all values from the map
-		 */
+	 * Remove all values from the map
+	 */
 	void clear()
 	{
 		deleteData_();
@@ -302,10 +302,10 @@ public:
 	}
 
 	/**
-		* Returns a new Object array of keys. This array will need to be freed in memory after use.
-		* NOTE: If a map has no keys, it will return an empty Object**
-		* @return new Object array of keys
-		*/
+	 * Returns a new Object array of keys. This array will need to be freed in memory after use.
+	 * NOTE: If a map has no keys, it will return an empty Object**
+	 * @return new Object array of keys
+	 */
 	virtual Object **get_keys()
 	{
 		Object **keys = new Object *[this->size()];
@@ -319,10 +319,10 @@ public:
 	}
 
 	/**
-		* Returns a new Object array of values. This array will need to be freed in memory after use.
-		* NOTE: If a map has no values, it will return an empty Object**
-		* @return new Object array of values
-		*/
+	 * Returns a new Object array of values. This array will need to be freed in memory after use.
+	 * NOTE: If a map has no values, it will return an empty Object**
+	 * @return new Object array of values
+	 */
 	virtual Object **get_values()
 	{
 		Object **vals = new Object *[this->size()];
@@ -349,41 +349,41 @@ public:
 	Map *helper_;
 
 	/**
-		 * Constructor
-		 */
+	 * Constructor
+	 */
 	MapStrObj()
 	{
 		this->helper_ = new Map();
 	}
 
 	/**
-		 * Constructor to tailor performance of map.
-		 * @param capacity number of buckets available to store different values.
-		 */
+	 * Constructor to tailor performance of map.
+	 * @param capacity number of buckets available to store different values.
+	 */
 	MapStrObj(size_t capacity)
 	{
 		this->helper_ = new Map();
 	}
 
 	/**
-		 * Deconstructor
-		 */
+	 * Deconstructor
+	 */
 	~MapStrObj()
 	{
 		delete this->helper_;
 	}
 
 	/**
-		 * Adds a mapping from key to val to the map. A key must be unique, if there is already a key
-		 * of the same value in the map, the value will be overwritten by the new value. Map takes 
-		 * ownership of this key and value
-		 * 
-		 * NOTE: This function will mutate the Map
-		 * 
-		 * @param key String key
-		 * @param val Object value
-		 * @return the old value from the given key, a nullptr is returned if no old value exists
-		 */
+	 * Adds a mapping from key to val to the map. A key must be unique, if there is already a key
+	 * of the same value in the map, the value will be overwritten by the new value. Map takes 
+	 * ownership of this key and value
+	 * 
+	 * NOTE: This function will mutate the Map
+	 * 
+	 * @param key String key
+	 * @param val Object value
+	 * @return the old value from the given key, a nullptr is returned if no old value exists
+	 */
 	Object *put(String *key, Object *val)
 	{
 		Object *prev = this->helper_->put(key, val);
@@ -391,22 +391,22 @@ public:
 	}
 
 	/**
-		 * Returns the object that the key maps to.
-		 * 
-		 * @param key String unique key
-		 * @return Object value for the key, a nullptr is returned if no value exists
-		 */
+	 * Returns the object that the key maps to.
+	 * 
+	 * @param key String unique key
+	 * @return Object value for the key, a nullptr is returned if no value exists
+	 */
 	Object *get(String *key)
 	{
 		return this->helper_->get(key);
 	}
 
 	/**
-		 * Removes the key value mapping from the map.
-		 * 
-		 * @param key String key of mapping to remove
-		 * @return Object value that was removed, a nullptr is returned if no value exists
-		 */
+	 * Removes the key value mapping from the map.
+	 * 
+	 * @param key String key of mapping to remove
+	 * @return Object value that was removed, a nullptr is returned if no value exists
+	 */
 	Object *remove(String *key)
 	{
 		Object *removed = this->helper_->remove(key);
@@ -414,20 +414,20 @@ public:
 	}
 
 	/**
-		 * Determines if the map contains the given key.
-		 * @param key String key
-		 * @return if the key is in the map
-		 */
+	 * Determines if the map contains the given key.
+	 * @param key String key
+	 * @return if the key is in the map
+	 */
 	bool contains_key(String *key)
 	{
 		return this->helper_->contains_key(key);
 	}
 
 	/**
-		* Returns a new Object array of keys. This array will need to be freed in memory after use.
-		* NOTE: If a map has no keys, it will return an empty Object**
-		* @return new Object array of keys
-		*/
+	 * Returns a new Object array of keys. This array will need to be freed in memory after use.
+	 * NOTE: If a map has no keys, it will return an empty Object**
+	 * @return new Object array of keys
+	 */
 	// Overrides Map.get_keys()
 	Object **get_keys() override
 	{
@@ -435,10 +435,10 @@ public:
 	}
 
 	/**
-		* Returns a new Object array of values. This array will need to be freed in memory after use.
-		* NOTE: If a map has no values, it will return an empty Object**
-		* @return new Object array of values
-		*/
+	 * Returns a new Object array of values. This array will need to be freed in memory after use.
+	 * NOTE: If a map has no values, it will return an empty Object**
+	 * @return new Object array of values
+	 */
 	// Overrides Map.get_values()
 	Object **get_values() override
 	{
@@ -446,9 +446,9 @@ public:
 	}
 
 	/**
-		 * @brief Determines the number of mappings in this map.
-		 * @return size of map
-		 */
+	 * @brief Determines the number of mappings in this map.
+	 * @return size of map
+	 */
 	// Overrides Map.size()
 	size_t size() override
 	{
@@ -456,13 +456,13 @@ public:
 	}
 
 	/**
-		 * @brief Determines if this map equals the given object. For two maps to be equal, they must
-		 * contain the exact same key-value pairs, BUT do not need to share the same locations inside
-		 * of the buckets.
-		 * 
-		 * @param other object to compare to
-		 * @return equals or not
-		 */
+	 * @brief Determines if this map equals the given object. For two maps to be equal, they must
+	 * contain the exact same key-value pairs, BUT do not need to share the same locations inside
+	 * of the buckets.
+	 * 
+	 * @param other object to compare to
+	 * @return equals or not
+	 */
 	// Overrides Map.equals
 	bool equals(Object *other) override
 	{
@@ -480,20 +480,20 @@ public:
 	}
 
 	/**
-		 * @brief Calculates the hash of the map. The hash will only be dependent on the key value pairs
-		 * of the map, and NOT the location of them in the buckets.
-		 * Example:
-		 * Map* map1 = new Map(10000);
-		 * Map* map2 = new Map();
-		 * Object* key = new Object();
-		 * Object* value = new Object();
-		 * map1->put(key, value);
-		 * map2->put(key, value);
-		 * // This should return true even though the key value pairs are in different bucket locations
-		 * map1->hash() == map2->hash(); 
-		 * 
-		 * @return hash representation of each key value pair in the map
-		 */
+	 * @brief Calculates the hash of the map. The hash will only be dependent on the key value pairs
+	 * of the map, and NOT the location of them in the buckets.
+	 * Example:
+	 * Map* map1 = new Map(10000);
+	 * Map* map2 = new Map();
+	 * Object* key = new Object();
+	 * Object* value = new Object();
+	 * map1->put(key, value);
+	 * map2->put(key, value);
+	 * // This should return true even though the key value pairs are in different bucket locations
+	 * map1->hash() == map2->hash(); 
+	 * 
+	 * @return hash representation of each key value pair in the map
+	 */
 	// Overrides Map.hash
 	size_t hash() override
 	{
